df5bfa6c186823dad5a20b916c11b43f4ff7cc4f
gganster <guilian.ganster@wisoftify.fr>
2021-11-16 11:38:53 +0100

    ✨ collapse component OK

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,9 +1,11 @@
 import Button from "./Button";
 import Spinner from "./Spinner";
 import Alert from "./Alert";
+import Collapse from "./Collapse";
 
 export {
   Button,
   Spinner,
-  Alert
+  Alert,
+  Collapse
 }
--- src/pages/dashboard/Overview.js
+++ src/pages/dashboard/Overview.js
@@ -1,46 +1,17 @@
+import {useState} from "react";
 import { Link } from "react-router-dom";
-import {Button, Spinner, Alert} from "lib/components";
+import {Button, Spinner, Alert, Collapse} from "lib/components";
 
 const Overview = () => {
+  const [collapse, setCollapse] = useState(false);
+
   return (
     <div className="p-5">
       <p>overview</p>
-      <Button color="primary" size="xs">XS Button</Button>
-      <Button color="primary" disabled>XS Button</Button><br />
-      <Button color="secondary" size="sm">SM Button</Button>
-      <Button color="secondary" disabled>SM Button</Button><br />
-      <Button color="success" size="md">MD Button</Button>
-      <Button color="success" disabled>MD Button</Button><br />
-      <Button color="danger" size="lg">LG Button</Button>
-      <Button color="danger" disabled>LG Button</Button><br />
-      <Alert>My Alert</Alert>
-      <Alert color="secondary">My Alert</Alert>
-      <Alert color="success">My Alert</Alert>
-      <Alert color="danger">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-      <Alert color="warning">My Alert</Alert>
-
+      <Button onClick={() => setCollapse(!collapse)}>{collapse ? "close" : "open"}</Button>
+      <Collapse unmount isOpen={collapse}>
+        <Alert>Test</Alert>
+      </Collapse>
       <div><Link to="/">to Login</Link></div>
     </div>
   )

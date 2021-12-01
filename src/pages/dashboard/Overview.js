eb9a20a685e02fee81c00bebe73e9378c502a2a1
gganster <guilian.ganster@wisoftify.fr>
2021-12-01 04:05:32 +0100

    add Font awesome Icon + debug scrollbar

--- src/pages/dashboard/Overview.js
+++ src/pages/dashboard/Overview.js
@@ -11,7 +11,45 @@ const Overview = () => {
       <Button onClick={() => setCollapse(!collapse)}>{collapse ? "close" : "open"}</Button>
       <Collapse unmount isOpen={collapse}>
         <Alert>Test</Alert>
+        <Alert>Test</Alert>
       </Collapse>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
+      <Alert>Test</Alert>
       <div><Link to="/">to Login</Link></div>
     </div>
   )
